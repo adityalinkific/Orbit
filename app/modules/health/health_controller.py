08d3f04ae031919291ab5b1bf3334151cccab892
Devashish Rajbhar <devashish0797@gmail.com>
2026-01-27 23:35:50 +0530

    Delete app directory

--- app/modules/health/health_controller.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.modules.health.health_services import HealthServices
-
-class Health():
-
-    @staticmethod
-    async def health_check(db: AsyncSession):
-        return await HealthServices.health_check(db)
-        
